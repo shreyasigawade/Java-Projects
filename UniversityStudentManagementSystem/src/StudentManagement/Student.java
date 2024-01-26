de1b8a2a4e1e53f6d2370cc5360f2ac9a9fb7d02
Shreyasi <shreyasigawade@gmail.com>
2024-01-26 19:10:20 +0530

    I am adding this file

---
+++ UniversityStudentManagementSystem/src/StudentManagement/Student.java
@@ -0,0 +1,58 @@
+package StudentManagement;
+
+public class Student {
+	private String StudentName;
+	private int age;
+	private String gender;
+	private double ContactNo;
+	private static String SchoolName;
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Student(String studentName, int age, String gender, double contactNo) {
+		
+		StudentName = studentName;
+		this.age = age;
+		this.gender = gender;
+		ContactNo = contactNo;
+	}
+	public String getStudentName() {
+		return StudentName;
+	}
+	public void setStudentName(String studentName) {
+		StudentName = studentName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public double getContactNo() {
+		return ContactNo;
+	}
+	public void setContactNo(double contactNo) {
+		ContactNo = contactNo;
+	}
+	public static String getSchoolName() {
+		return SchoolName;
+	}
+	public static void setSchoolName(String schoolName) {
+		SchoolName = schoolName;
+	}
+	@Override
+	public String toString() {
+		return "Student [StudentName=" + StudentName + ", age=" + age + ", gender=" + gender + ", ContactNo="
+				+ ContactNo + "]";
+	}
+	
+	
+}
+		
---
+++ UniversityStudentManagementSystem/src/StudentManagement/StudentDaoImpl.java
@@ -0,0 +1,63 @@
+package StudentManagement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDaoImpl implements StudentDao {
+       Student arr[]=new Student[10];
+       int index=0;
+       
+	@Override
+	public boolean addStudent(Student student) {
+		if(index<arr.length) {
+			arr[index++]=student;
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean updateStudent(Student student) {
+		for(int i=0;i<index;i++) {
+			if(arr[i]!=null && arr[i].getStudentName().equals(student.getStudentName())) {
+				arr[index]=student;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean deleteStudent(String name) {
+		
+		for(int i=0;i<index;i++) {
+			if(arr[i]!=null && arr[i].getStudentName().equals(name)) {
+				arr[i]=null;
+				return true;
+			}
+			}
+		return false;
+	}
+
+	@Override
+	public Student[] viewAllStudents() {
+		
+		
+		return arr;
+		
+	}
+
+	@Override
+	public Student getstudentbyname(String name) {
+		Student students=null;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]!=null && arr[i].getStudentName().equals(name)) {
+				return arr[i];
+			}
+		}
+		return null;
+	}
+
+	
+
+}
